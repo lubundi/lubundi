7cc33fbb2870f22478b8e8fe7ec5d3fdadde17fb
Zohan Haque <161150779+ZohanHaqu@users.noreply.github.com>
2025-08-06 15:39:25 -0400

    Update mainloop.py

--- lubundi/mainloop.py
+++ lubundi/mainloop.py
@@ -1,19 +1,18 @@
-from ursina import Ursina, window
+from ursina import Ursina, window, Func
 
 class MainLoop:
     def __init__(self, update_func=None):
-        # Initialize Ursina app
         self.app = Ursina()
-
-        # Change window title to "Lubundi"
-        window.title = "Lubundi"
-
-        # Store optional update callback
+        window.title = "Lubundi"  # Set Lubundi title
         self.update_func = update_func
+
+        # Attach the update function via Ursina's global update
         if self.update_func:
-            from ursina import update
-            update.append(self.update_func)
+            # Ursina calls update functions automatically every frame if they're global
+            # We'll create a small Entity just to call our function every frame
+            from ursina import Entity
+            self.updater = Entity()
+            self.updater.update = self.update_func
 
     def run(self):
-        # Start game loop
         self.app.run()
