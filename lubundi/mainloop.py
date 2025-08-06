136d633f8aacd88fecad563cc7f25c4c23205c42
Zohan Haque <161150779+ZohanHaqu@users.noreply.github.com>
2025-08-06 15:34:25 -0400

    Update mainloop.py
    Fixed Traceback (most recent call last):
      File "C:\Users\zohan\OneDrive\Desktop\capella.py", line 17, in <module>
        loop = MainLoop(update_func=update)
      File "C:\Python313\Lib\site-packages\lubundi\mainloop.py", line 14, in __init__
        from ursina import update
    ImportError: cannot import name 'update' from 'ursina' (C:\Python313\Lib\site-packages\ursina\__init__.py)

--- lubundi/mainloop.py
+++ lubundi/mainloop.py
@@ -1,18 +1,22 @@
-from ursina import Ursina
+from ursina import Ursina, Func
 
 class MainLoop:
     def __init__(self, update_func=None):
         """
         MainLoop initializes Ursina and handles automatic frame updates.
-        :param update_func: a function to call every frame (like updating player, camera, etc.)
+        :param update_func: a function to call every frame
         """
         self.app = Ursina()
         self.update_func = update_func
 
-        # Register the update callback if provided
+        # If an update function is provided, register it
         if self.update_func:
-            from ursina import update
-            update.append(self.update_func)
+            # Ursina automatically calls 'update' functions; we can wrap it using Func
+            from ursina import Entity
+            class Updater(Entity):
+                def update(self_inner):
+                    self.update_func()
+            Updater()
 
     def run(self):
         """Start the game loop"""
